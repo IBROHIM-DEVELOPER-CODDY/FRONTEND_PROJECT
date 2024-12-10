92024f55de251b72bd59f8d77bb957588e3df9bf
IBROHIM <IBROHIM-DEVELOPER-CODDY>
2024-12-10 16:47:00 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <marquee behavior="" direction=""><h1>hell</h1></marquee>
+    </div>
+  )
+}
+
+export default App
+<marquee behavior="" direction=""><h1>hell</h1></marquee>
